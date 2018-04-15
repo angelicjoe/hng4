ed4dedfe41a85cfecbba5f42eda92503ad6dd634
angelicjoe <angelicjoekeyz@gmail.com>
2018-04-15 19:24:57 +0100

    Add files via upload

---
+++ home.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HOME</title>
+	
+</head>
+<body style="background-image: url(4.jpg);background-position: fixed; background-repeat: none; background-size: cover ">
+	<button style="color: black; font-size: 20px;"><b>Hi CodeAngelic</b></button> <br><br><br>
+
+	<center>
+		<p>
+			<b>
+				<h1 style="color:white; font-family: Arial, Helvetica, sans-serif; font-size: 50px;"> WELCOME TO HNG INTERNSHIP 4</h1>
+			</b>
+		</p>
+	</center>
+
+
+
+	<center><b><h1>You're currently in #Stage1</h1><b><br>
+		<p style="font-size: 20px"><b>Please submit your task below</b></p>
+		<input style="color: black; font-size: 20px;" type="file" name=""><br><br><br><hr>
+
+		<p style="color: black; font-size: 20px;">Please note the time of your submission</button></center>
+
+			<center><button style="color:red; font-size: 60px;"><?php echo date("H:iA"); ?></button></center>
+
+
+</body>
+
+</html>
